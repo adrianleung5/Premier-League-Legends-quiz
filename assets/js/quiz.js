5fb08f04f4e1352c0d92fc43745559b072ce6866
adrianleung5 <adrianleung50@gmail.com>
2022-11-20 14:11:20 +0000

    add variables to quiz in js

---
+++ assets/js/quiz.js
@@ -0,0 +1,4 @@
+const welcome = document.getElementById("welcome");
+const checkInput = document.getElementById("quiz_username");
+const quizButton = document.getElementById("quiz_button")
+const modalBox = document.getElementById("username");
