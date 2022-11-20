8a22490dd9f6d3b4aa6e714d1fecb15e2d90d2c5
adrianleung5 <adrianleung50@gmail.com>
2022-11-20 14:13:06 +0000

    add more variables to quiz

--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -2,3 +2,11 @@ const welcome = document.getElementById("welcome");
 const checkInput = document.getElementById("quiz_username");
 const quizButton = document.getElementById("quiz_button")
 const modalBox = document.getElementById("username");
+const nextButton =document.getElementById("next_button");
+const quizSection =document.getElementById("quiz") ; 
+
+const question = document.getElementById("question");
+const optionA = document.getElementById("optionA");
+const optionB = document.getElementById("optionB");
+const optionC = document.getElementById("optionC");
+const optionD = document.getElementById("optionD")
