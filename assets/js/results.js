a6aa37e28a7085efff2490e6498f9ad18342f434
adrianleung5 <adrianleung50@gmail.com>
2022-11-24 22:25:05 +0000

    create variables for result, username, score, results note, back to home and trophy

---
+++ assets/js/results.js
@@ -0,0 +1,6 @@
+const result = +localStorage.getItem("result");
+const username = localStorage.getItem("username");
+const score = document.getElementById("score");
+const resultSection =document.getElementById("results_note");
+const navigate = document.getElementById("back_to_home");
+const trophy = document.getElementById("trophy");
